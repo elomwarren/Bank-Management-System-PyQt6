aa85f75959845a741dae5d9e3900413a422aada4
Elom Warren <elom.kodjoh@gmail.com>
2023-08-08 23:16:07 +0000

    4th commit - create parent class dashboard to optimize code

--- win_02_1_CusServDashboard.py
+++ win_02_1_CusServDashboard.py
@@ -1,3 +1,5 @@
+import typing
+from PyQt6 import QtCore
 from PyQt6.QtWidgets import (
     QApplication,
     QMainWindow,
@@ -25,12 +27,12 @@ import qdarktheme
 import sys
 
 
-class cusServDashboard(QMainWindow):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+# Construction of class dashboard, blueprint for Customer Service and HR dashboards
+class dashboard(QMainWindow):
+    def __init__(self, windowTitle: str, entities: dict) -> None:
+        super().__init__()
 
         # set the window title
-        windowTitle = "Customer Service Dashboard"
         self.setWindowTitle(windowTitle)
 
         # set WINDOW ICON (icons from icons8.com)
@@ -47,19 +49,18 @@ class cusServDashboard(QMainWindow):
         # initilize other windows member to None
         self.welcome = None
         self.queryInterface = None
-        self.customers = None
-        self.accounts = None
-        self.cards = None
-        self.transactions = None
-        self.loans = None
-        self.loansPayment = None
+        # department specific entities
+        self.entities = entities
+
 
         # CREATE THE USER INTERFACE
         self.initUI()
 
-    # user interface function
     def initUI(self):
-        ######################### ADD WIDGETS #########################
+        """
+        Initializes the customers window.
+        """
+        ######################### CREATE WIDGETS #########################
 
         # Dashboard label
         self.dashboardLabel = QLabel("DASHBOARD")
@@ -67,23 +68,15 @@ class cusServDashboard(QMainWindow):
         self.dashboardLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         # Entities Label
-        self.entitiesLabel = QLabel("Entities")
-        self.entitiesLabel.setFont(QFont("Century", 16))
-        self.entitiesLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.label = QLabel("Entities")
+        self.label.setFont(QFont("Century", 16))
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         ### Push buttons for entities ###
-
-        self.cusButton = QPushButton("Customers", clicked=lambda: self.open_customers())  # type: ignore
-
-        self.accButton = QPushButton("Accounts", clicked=lambda: self.open_accounts())  # type: ignore
-
-        self.cardButton = QPushButton("Cards", clicked=lambda: self.open_cards())  # type: ignore
-
-        self.txnButton = QPushButton("Transactions", clicked=lambda: self.open_transactions())  # type: ignore
-
-        self.loanButton = QPushButton("Loans", clicked=lambda: self.open_loans())  # type: ignore
-
-        self.loanPayButton = QPushButton("Loans Payment", clicked=lambda: self.open_loansPayment())  # type: ignore
+        self.buttons = [
+            QPushButton(entity, clicked=lambda: self.open(self.entities[entity]))  # type: ignore
+            for entity in self.entities
+        ]
 
         # TOOL LABEL
         self.toolLabel = QLabel("Tools")
@@ -115,15 +108,11 @@ class cusServDashboard(QMainWindow):
         layout.addSpacing(15)
 
         # Entities Label
-        layout.addWidget(self.entitiesLabel)
+        layout.addWidget(self.label)
 
         # Push buttons for entities
-        layout.addWidget(self.cusButton)
-        layout.addWidget(self.accButton)
-        layout.addWidget(self.cardButton)
-        layout.addWidget(self.txnButton)
-        layout.addWidget(self.loanButton)
-        layout.addWidget(self.loanPayButton)
+        for button in self.buttons:
+            layout.addWidget(button)
 
         # Tool label
         layout.addWidget(self.toolLabel)
@@ -143,10 +132,6 @@ class cusServDashboard(QMainWindow):
         ### SET CONTENT MARGINS ###
         layout.setContentsMargins(250, 150, 250, 75)  # left, top, right, bottom
 
-        # Set the layout
-        # self.setLayout(layout)
-        ########################################
-
         ### Center window content ###
         container = QWidget()
         container.setLayout(layout)
@@ -155,52 +140,19 @@ class cusServDashboard(QMainWindow):
 
     ##################### BUTTON FUNCTIONS #####################
 
-    def open_customers(self):
-        # print('Customers')
-        self.customers = customers()
+    def open(self, entityWindow):
+        window = entityWindow()
         self.hide()
-        self.customers.show()
-
-    def open_accounts(self):
-        # print('Accounts')
-        self.accounts = accounts()
-        self.hide()
-        self.accounts.show()
-
-    def open_cards(self):
-        # print('Cards')
-        self.cards = cards()
-        self.hide()
-        self.cards.show()
-
-    def open_transactions(self):
-        # print('Transactions')
-        self.transactions = transactions()
-        self.hide()
-        self.transactions.show()
-
-    def open_loans(self):
-        # print('Loans')
-        self.loans = loans()
-        self.hide()
-        self.loans.show()
-
-    def open_loansPayment(self):
-        # print('Loans Payment')
-        self.loansPayment = loansPayment()
-        self.hide()
-        self.loansPayment.show()
+        window.show()
 
     # Push button for Data Query Wizard
     def dataQueryWizard(self):
-        # print('Data Query Wizard')
         self.queryInterface = queryInterface()
         self.hide()
         self.queryInterface.show()
 
     # Logout Push button
     def logout(self):
-        # print('Logout')
         from win_01_WelcomeLogin import welcome
 
         self.welcome = welcome()
@@ -228,6 +180,26 @@ class cusServDashboard(QMainWindow):
         self.move(x, y)
 
     ############################# END OF CENTER FUNCTION #############################
+
+    # END OF dashboard class
+
+
+csEntities = {
+    "Customers": customers,
+    "Accounts": accounts,
+    "Cards": cards,
+    "Transactions": transactions,
+    "Loans": loans,
+    "Loan Payments": loansPayment
+}
+
+
+class cusServDashboard(dashboard):
+    def __init__(self):
+        super().__init__(
+            windowTitle= "Customer Service Dashboard",
+            entities= csEntities
+        )
 
 
 if __name__ == "__main__":
